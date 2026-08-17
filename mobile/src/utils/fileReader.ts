4efab58f798fe861c8bcda50c3a199bfcee97c2e
proviewers <proviewers@gmail.com>
2026-08-17 18:08:30 +0530

    fix(utils): match readTextFile signature between native and web

--- mobile/src/utils/fileReader.ts
+++ mobile/src/utils/fileReader.ts
@@ -1,5 +1,5 @@
 import RNFS from 'react-native-fs';
 
-export const readTextFile = async (uri: string): Promise<string> => {
+export const readTextFile = async (uri: string, _fileObject?: any): Promise<string> => {
   return RNFS.readFile(uri.replace('file://', ''), 'utf8');
 };
